36f3a12363522537a9db7aa341650a1d1d8c377f
trichoplax <xalpohcirt@gmail.com>
2020-11-17 20:28:42 +0000

    Extract square colour function into a module

--- docs/index.js
+++ docs/index.js
@@ -1,23 +1,11 @@
+import {squareColour} from './square_colour.js'
+
 const canvas = document.getElementById('polipod_canvas')
 const context = canvas.getContext('2d')
 
 let red = 0
 let green = 0
 let blue = 0
-
-const squareColour = colour => {
-    [red, green, blue] = colour
-
-    red += Math.floor(Math.random() * 2)
-    green += Math.floor(Math.random() * 2)
-    blue += Math.floor(Math.random() * 2)
-
-    red %= 256
-    green %= 256
-    blue %= 256
-
-    return [red, green, blue]
-}
 
 const renderLoop = () => {
     [red, green, blue] = squareColour([red, green, blue])
