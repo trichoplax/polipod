2b44141c8e8a5432caffeae23c3e39c605ce23bd
trichoplax <xalpohcirt@gmail.com>
2020-11-23 22:34:25 +0000

    Move renderLoop into separate module

--- docs/index.js
+++ docs/index.js
@@ -1,13 +1,7 @@
-import {squareColour} from 'https://unpkg.com/@trichoplax/polipod_colours_js@0.0.0/index.js'
-import {squareAngle} from 'https://unpkg.com/@trichoplax/polipod_angles_js@0.0.0/index.js'
+import {renderLoop} from 'https://unpkg.com/@trichoplax/polipod_render_js@0.0.0/index.js'
 
 const canvas = document.getElementById('polipod_canvas')
 const context = canvas.getContext('2d')
-
-let red = 0
-let green = 0
-let blue = 0
-let angle = 0
 
 const changeColourCheckbox = document.getElementById('change_colour')
 let changeColourActive = changeColourCheckbox.checked
@@ -20,20 +14,5 @@ let changeAngleActive = changeAngleCheckbox.checked
 changeAngleCheckbox.addEventListener('change', () => {
     changeAngleActive = changeAngleCheckbox.checked
 })
-
-const renderLoop = () => {
-    if (changeColourActive) {
-        [red, green, blue] = squareColour([red, green, blue])
-    }
-
-    if (changeAngleActive) {
-        angle = squareAngle(angle)
-    }
-
-    context.fillStyle = `rgb(${red}, ${green}, ${blue})`
-    context.fillRect(angle + 10, 10, 50, 50)
-
-    window.requestAnimationFrame(renderLoop)
-}
 
 window.requestAnimationFrame(renderLoop)
