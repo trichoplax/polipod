58b2f73a17649d50e3240a9c43f32988b2712fab
trichoplax <xalpohcirt@gmail.com>
2020-11-23 21:10:30 +0000

    Add angle change checkbox and module

--- docs/index.js
+++ docs/index.js
@@ -1,4 +1,5 @@
 import {squareColour} from 'https://unpkg.com/@trichoplax/polipod_colours_js@0.0.0/index.js'
+import {squareAngle} from 'https://unpkg.com/@trichoplax/polipod_angles_js@0.0.0/index.js'
 
 const canvas = document.getElementById('polipod_canvas')
 const context = canvas.getContext('2d')
@@ -6,6 +7,7 @@ const context = canvas.getContext('2d')
 let red = 0
 let green = 0
 let blue = 0
+let angle = 0
 
 const changeColourCheckbox = document.getElementById('change_colour')
 let changeColourActive = changeColourCheckbox.checked
@@ -13,12 +15,23 @@ changeColourCheckbox.addEventListener('change', () => {
     changeColourActive = changeColourCheckbox.checked
 })
 
+const changeAngleCheckbox = document.getElementById('change_angle')
+let changeAngleActive = changeAngleCheckbox.checked
+changeAngleCheckbox.addEventListener('change', () => {
+    changeAngleActive = changeAngleCheckbox.checked
+})
+
 const renderLoop = () => {
     if (changeColourActive) {
         [red, green, blue] = squareColour([red, green, blue])
     }
+
+    if (changeAngleActive) {
+        angle = squareAngle(angle)
+    }
+
     context.fillStyle = `rgb(${red}, ${green}, ${blue})`
-    context.fillRect(10,10,50,50)
+    context.fillRect(angle + 10, 10, angle + 50, 50)
 
     window.requestAnimationFrame(renderLoop)
 }
