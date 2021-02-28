aaf1d1cdaf54c53098ede0470fd1d7ac29a3ad4e
trichoplax <xalpohcirt@gmail.com>
2021-02-28 21:45:28 +0000

    Move code from here to polipod_render_js repo to keep variables in scope

--- src/index.js
+++ src/index.js
@@ -1,18 +1,3 @@
 import {renderLoop} from '@trichoplax/polipod_render_js/index.js'
 
-const canvas = document.getElementById('polipod_canvas')
-const context = canvas.getContext('2d')
-
-const changeColourCheckbox = document.getElementById('change_colour')
-let changeColourActive = changeColourCheckbox.checked
-changeColourCheckbox.addEventListener('change', () => {
-    changeColourActive = changeColourCheckbox.checked
-})
-
-const changeAngleCheckbox = document.getElementById('change_angle')
-let changeAngleActive = changeAngleCheckbox.checked
-changeAngleCheckbox.addEventListener('change', () => {
-    changeAngleActive = changeAngleCheckbox.checked
-})
-
 window.requestAnimationFrame(renderLoop)
